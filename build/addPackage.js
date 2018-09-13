b342742c755754747176161531027a07fe650bb2
douchi <douchi@2dfire.com>
2018-09-13 10:49:14 +0800

    ..

--- build/addPackage.js
+++ build/addPackage.js
@@ -33,12 +33,12 @@ export default {
 // 读取packages下面的所有组件名称
 fs.readdir(packagesPath, function (err, files) {
   if(err) {throw err}
-  if(!files.includes(packageName)){
+  if(!files.includes(tranformPackageName)){
     // 在packages目录下面创建包
-    fs.mkdirSync(`${packagesPath}/${packageName}`, '0777')
-    fs.mkdirSync(`${packagesPath}/${packageName}/src`, '0777')
-    fs.writeFileSync(`${packagesPath}/${packageName}/index.js`, templateJs)
-    fs.writeFileSync(`${packagesPath}/${packageName}/src/${tranformPackageName}.vue`, templateVue)
+    fs.mkdirSync(`${packagesPath}/${tranformPackageName}`, '0777')
+    fs.mkdirSync(`${packagesPath}/${tranformPackageName}/src`, '0777')
+    fs.writeFileSync(`${packagesPath}/${tranformPackageName}/index.js`, templateJs)
+    fs.writeFileSync(`${packagesPath}/${tranformPackageName}/src/${tranformPackageName}.vue`, templateVue)
 
     // 在doc/views/content目录下生成文档
     fs.writeFileSync(`${docPath}/views/content/${tranformPackageName}.md`, '请在这里输入组件文档内容')
@@ -46,7 +46,7 @@ fs.readdir(packagesPath, function (err, files) {
     // 在demo/views下面生成案例组件
     fs.writeFileSync(`${demoPath}/views/${tranformPackageName}.vue`, templateVue)
 
-    files.push(packageName)
+    files.push(tranformPackageName)
     packageIndexJs(files)
     demoRouterIndexJs(files)
     docRouterIndexJs(files)
