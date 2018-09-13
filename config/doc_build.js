692a30c7bab6ab336ad455a784c7060b61e794b6
douchi <douchi@2dfire.com>
2018-09-13 11:24:51 +0800

    .

--- config/doc_build.js
+++
@@ -1,4 +0,0 @@
-export default {
-  IFRAME_URL: 'http://10.1.6.28:2233/fed/static/doc/component-ui/demo/index.html#',
-  JUMP_URL: 'http://10.1.6.28:2233/fed/static/doc/container-ui/doc/index.html#'
-}
--- config/doc_dev.js
+++
@@ -1,4 +0,0 @@
-export default {
-  IFRAME_URL: 'http://' + localIp + ':8000/index.html#',
-  JUMP_URL: 'http://' + localIp + ':4000/#/'
-}
